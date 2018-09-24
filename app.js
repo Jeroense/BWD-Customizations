62baf00daa1e87fcd0b3b0836230aa512dd9fb14
Jeroense <jeroense@hotmail.com>
2018-09-24 15:11:02 +0200

    Added image save function

--- app.js
+++ app.js
@@ -3,6 +3,55 @@ var baseHeight = 0;
 var customWidth = 0;
 var customHeight = 0;
 var stage;
+var lineCounter;
+
+function addLines(group, p1, p2, p3, p4, name) {
+    var helpLine = new Konva.Line({
+        points: [p1, p2, p3, p4],
+        name: name,
+        stroke: 'blue',
+        dash: [1,5],
+        strokeWidth: 1,
+        id: name
+    });
+    group.add(helpLine);
+}
+
+function setLineHit(lines){
+    lines.attrs.stroke = 'blue';
+    lines.attrs.dash = [1,2];
+}
+
+function setLineMiss(lines){
+    lines.attrs.stroke = 'red';
+    lines.attrs.dash = [1,5];
+}
+
+function setLineColor(img) {
+    var width = img.getWidth() * img.getScaleX();
+    var height = img.getHeight() * img.getScaleY();
+    var horizontalCenter = parseInt(img.getX() + (width / 2));
+    var verticalCenter = parseInt(img.getY() + (height / 2));
+
+    var lines;
+    lines = stage.find('#vCenter')[0];
+    horizontalCenter == parseInt(baseWidth / 2) ? setLineHit(lines) : setLineMiss(lines);
+
+    lines = stage.find('#vLeft')[0];
+    horizontalCenter == parseInt(baseWidth / 3) ? setLineHit(lines) : setLineMiss(lines);
+
+    lines = stage.find('#vRight')[0];
+    horizontalCenter == parseInt(baseWidth - (baseWidth / 3)) ? setLineHit(lines) : setLineMiss(lines);
+
+    lines = stage.find('#hCenter')[0];
+    verticalCenter == parseInt(baseHeight / 2) ? setLineHit(lines) : setLineMiss(lines);
+
+    lines = stage.find('#hTop')[0];
+    verticalCenter == parseInt(baseHeight / 3) ? setLineHit(lines) : setLineMiss(lines);
+
+    lines = stage.find('#hBottom')[0];
+    verticalCenter == parseInt(baseHeight - (baseHeight / 3)) ? setLineHit(lines) : setLineMiss(lines);
+}
 
 function drawImage() {
     stage = new Konva.Stage({
@@ -16,9 +65,7 @@ function drawImage() {
     var baseImage = new Konva.Image({
         image: baseObj,
         width: baseWidth,
-        height: baseHeight,
-        draggable: false,
-        
+        height: baseHeight
     });
     layer.add(baseImage);
 
@@ -32,9 +79,64 @@ function drawImage() {
         draggable: true,
         visible: true
     });
-    layer.add(customImage);
 
+    // add cursor styling
+    customImage.on('mouseover', function() {
+        document.body.style.cursor = 'pointer';
+    });
+    customImage.on('mouseout', function() {
+        document.body.style.cursor = 'default';
+    });
+
+    customImage.on('dragmove', function() {
+        setLineColor(this);
+    });
+
+    baseGroup = new Konva.Group({
+        draggable: false
+    });
+
+    linesGroup = new Konva.Group({
+
+    });
+
+    addLines(linesGroup, (baseWidth / 2), 0, (baseWidth / 2), baseHeight, 'vCenter');
+    addLines(linesGroup, (baseWidth / 3), 0, (baseWidth / 3), baseHeight, 'vLeft');
+    addLines(linesGroup, baseWidth - (baseWidth / 3), 0, baseWidth - (baseWidth / 3), baseHeight, 'vRight');
+    addLines(linesGroup, 0, (baseHeight / 2), baseWidth, (baseHeight / 2), 'hCenter');
+    addLines(linesGroup, 0, (baseHeight / 3), baseWidth, (baseHeight / 3), 'hTop');
+    addLines(linesGroup, 0, baseHeight - (baseHeight / 3), baseWidth, baseHeight - (baseHeight / 3), 'hBottom');
+
+    baseGroup.add(customImage);
+    baseGroup.add(linesGroup);
+    layer.add(baseGroup);
     stage.add(layer);
+
+    document.querySelector('#saveImage').addEventListener('click', function(){
+        linesGroup.destroy();
+        layer.draw();
+    });
+
+    container.tabIndex = 1;
+    container.focus();
+    container.addEventListener('keydown', function(e) {
+        var xPosition = customImage.getX();
+        var yPosition = customImage.getY();
+        var value = e.keyCode;
+        if (value === 38) {
+            customImage.setY(yPosition - 1);
+        } else if (value === 40) {
+            customImage.setY(yPosition + 1);
+        } else if (value === 37) {
+            customImage.setX(xPosition - 1);
+        } else if (value === 39) {
+            customImage.setX(xPosition + 1);
+        } else {
+            return;
+        }
+        setLineColor(customImage);
+        layer.draw();
+    });
 
     stage.on('click tap', function (e) {
         // if click on empty area - remove all transformers
@@ -45,6 +147,8 @@ function drawImage() {
         }
         // do nothing if clicked outside of customImage
         if (!e.target.hasName('customization')) {
+            stage.find('Transformer').destroy();
+            layer.draw();
             return;
         }
 
@@ -58,17 +162,16 @@ function drawImage() {
         tr.attachTo(e.target);
         layer.draw();
     });
-
 }
 
 var baseObj = new Image();
 baseObj.src = 'images/tshirt-white.png';
 baseObj.onload = function() {
-    drawImage(this);
+    layer.draw();
 };
 
 var customObj = new Image();
-customObj.src = 'images/lips.png';
+customObj.src = 'images/print.png';
 customObj.onload = function() {
     ratio = this.width / this.height
     customHeight = this.height;
